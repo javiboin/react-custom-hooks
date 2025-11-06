832f02e8503290cbf66133e5c3c6ac592ab121ca
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-11-06 17:22:10 -0300

    Init: Subimos 3 Hooks personalizados sobre Contadores, Formularios y Fetch

---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,16 @@
+import { useCounter } from "../hooks/useCounter"
+
+export const CounterApp = () => {
+    
+    const { counter, increment, reset, decrement } = useCounter(0)
+
+    return (
+    <>
+        <h1>Contador</h1>
+        <h4>{ counter }</h4>
+        <button className="btn btn-primary" onClick={() => increment(1)}>Sumar</button>
+        <button className="btn btn-danger" onClick={() => reset()}>Reset</button>
+        <button className="btn btn-primary" onClick={() => decrement(1, false)}>Restar</button>
+    </>
+  )
+}
